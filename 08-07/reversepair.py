2f304ccf5070b0dc7eac09bb092bc20af568c156
fatima72 <aseyafatima72@gmail.com>
2021-07-08 23:07:54 +0530

    change in sum of two

--- 08-07/reversepair.py
+++ 08-07/reversepair.py
@@ -8,28 +8,30 @@ nums[i] > 2 * nums[j].
 """
 
 
-nums = list(map(int,input().split()))
-
-
+nums = list(map(int, input().split()))
 
 
 def reversePairs(nums):
     ans = [0]
+
     def merge(l):
-        if len(l)<=1:
+        if len(l) <= 1:
             return l
         list_A = merge(l[:len(l)//2])
         list_B = merge(l[len(l)//2:])
-        
+
         left = right = 0
-    
-        while(left<len(list_A) and right<len(list_B)):
-            if list_A[left]<=2*list_B[right]:
-                left+=1
+
+        while(left < len(list_A) and right < len(list_B)):
+            if list_A[left] <= 2*list_B[right]:
+                left += 1
             else:
-                ans[0]+=len(list_A)-left
-                right+=1
+                ans[0] += len(list_A)-left
+                right += 1
 
         return sorted(list_A+list_B)
     merge(nums)
     return ans[0]
+
+
+print(reversePairs(nums))
--- 08-07/sumoftwo.py
+++ 08-07/sumoftwo.py
@@ -8,21 +8,22 @@ You can return the answer in any order.
 """
 
 
-nums = list(map(int,input().split()))
+nums = list(map(int, input().split()))
 
 target = int(input())
 
 
-def sumoftwo(nums,target):
-    s=0
+def sumoftwo(nums, target):
+    s = 0
     for num in range(len(nums)):
-        if(nums[num]<target):
-            s= target - nums[num]
-            for j in range(len(nums[num+1:])):
-                if nums[j]==s:
-                    return (num,j)
+        if(nums[num] < target):
+            s = target - nums[num]
+
+            for j in range(num+1, len(nums[num:])):
+
+                if nums[j] == s:
+                    return (num, j)
     return
 
-print(sumoftwo(nums,target))     
-            
-          
+
+print(sumoftwo(nums, target))
